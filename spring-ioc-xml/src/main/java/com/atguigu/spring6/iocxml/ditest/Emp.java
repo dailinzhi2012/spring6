1a7bae78a141411eb119b5d0b5955cb95f9321d2
dailinzhi <dailinzhi2012@163.com>
2024-12-17 16:07:34 +0800

    2024年12月17日16:07:18

--- spring-ioc-xml/src/main/java/com/atguigu/spring6/iocxml/ditest/Emp.java
+++ spring-ioc-xml/src/main/java/com/atguigu/spring6/iocxml/ditest/Emp.java
@@ -1,5 +1,53 @@
 package com.atguigu.spring6.iocxml.ditest;
 
+import java.util.Arrays;
+
 public class Emp {
+
+    //对象类型属性：表明员工属于某个部门
+    private Dept dept;
+
     private String ename;
+    private Integer age;
+
+    //爱好
+    private String[] hobby;
+
+    public String[] getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String[] hobby) {
+        this.hobby = hobby;
+    }
+
+    public Dept getDept() {
+        return dept;
+    }
+
+    public void setDept(Dept dept) {
+        this.dept = dept;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void work(){
+        System.out.println(ename+" emp working..."+age);
+        dept.info();
+        System.out.println(Arrays.toString(hobby));
+    }
 }
